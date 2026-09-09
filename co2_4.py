c937e43d208ca6e71f560889585f6ba654c1bced
aswinsasidhar <aaswinsasidhar@gmail.com>
2026-09-09 11:18:36 +0530

    Add files via upload

---
+++ co2_4.py
@@ -0,0 +1,30 @@
+
+for num in range(1000,10000):
+        if all(int(digit)%2==0 for digit in str(num)):
+            root=int(num**0.5)
+            if root*root==num:
+                print(         num)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
